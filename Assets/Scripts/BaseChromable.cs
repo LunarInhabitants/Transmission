0f7c867ea386b57a1070df266cbb3990c0bbf87c
danm36 <dan36masterson@hotmail.com>
2018-01-26 22:30:00 +0000

    CHIKE

--- Assets/Scripts/BaseChromable.cs
+++ Assets/Scripts/BaseChromable.cs
@@ -10,19 +10,17 @@ public class BaseChromable : MonoBehaviour
     [SerializeField, Range(0.0f, 1.0f)] private float _b = 1.0f;
     public float R { get { return _r; } set { _r = Mathf.Clamp01(value); } }
     public float G { get { return _g; } set { _g = Mathf.Clamp01(value); } }
-    public float B { get { return _b; } set { _g = Mathf.Clamp01(value); } }
+    public float B { get { return _b; } set { _b = Mathf.Clamp01(value); } }
 
     protected new Renderer renderer;
 
     protected virtual void Start()
     {
-        renderer = GetComponent<Renderer>();
+        renderer = GetComponentInChildren<Renderer>();
     }
 
     protected virtual void Update ()
     {
-        //R = Mathf.Sin(Time.time) * 0.5f + 0.5f;
-        //G = Mathf.Cos(Time.time) * 0.5f + 0.5f;
         renderer.material.SetColor("_Color", new Color(_r, _g, _b));
     }
 }
---
+++ Assets/Scripts/ChickenController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenController : BaseChromable
+{
+    Animator animator;
+
+    float animOffset;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponentInChildren<Animator>();
+        switch (Random.Range(0, 3))
+        {
+            default:
+            case 0:
+                R = 1.0f;
+                G = 1.0f;
+                B = 0.0f;
+                break;
+            case 1:
+                R = 1.0f;
+                G = 0.0f;
+                B = 1.0f;
+                break;
+            case 2:
+                R = 0.0f;
+                G = 1.0f;
+                B = 1.0f;
+                break;
+        }
+
+        animOffset = Random.Range(0.0f, 5.0f);
+    }
+
+    protected override void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Sin(Time.time + animOffset));
+        if ((Time.time + animOffset) % 3.0f < 0.1f)
+            animator.SetTrigger("IsAttacking");
+        base.Update();
+    }
+}
