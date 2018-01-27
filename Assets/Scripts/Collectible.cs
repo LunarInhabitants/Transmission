660c80611b39239a610ed01432136a3f2c7303f1
Conor Colgan <conorcolgan10@gmail.com>
2018-01-27 22:06:59 +0000

    GIMME DAT

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private float y;
+
+    // Use this for initialization
+    void Start()
+    {
+        y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * 100, Space.World);
+        transform.position = new Vector3(transform.position.x, y + Mathf.Sin(Time.time) / 2, transform.position.z);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<Movement>())
+        {
+
+        }
+    }
+}
--- Assets/Scripts/SpiralBoi.cs
+++ Assets/Scripts/SpiralBoi.cs
@@ -8,6 +8,9 @@ public class SpiralBoi : MonoBehaviour
     public  Vector3 _endPoint = new Vector3(0, 0, 10);
 
     [SerializeField] private int _resolution = 50;
+    [SerializeField] private float _var1 = 4.0f;
+    [SerializeField] private float _var2 = 0.1f;
+    [SerializeField] private float _var3 = 0.02f;
 
     private LineRenderer _lineRenderer;
     private List<Vector3> positions;
@@ -31,12 +34,21 @@ public class SpiralBoi : MonoBehaviour
 	    if (_lineRenderer.enabled)
 	    {
 	        Vector3 movementVector = _endPoint - _startPoint;
+	        _resolution = (int)((float)movementVector.magnitude / _var3);
+	        _lineRenderer.positionCount = _resolution;
+	        if (positions.Count < _resolution)
+	        {
+	            for (int i = positions.Count; i <= _resolution; i++)
+	            {
+                    positions.Add(new Vector3());
+	            }
+	        }
 	        float distance = movementVector.magnitude / _resolution;
 	        for (int i = 0; i < positions.Count; i++)
 	        {
 	            positions[i] = new Vector3(
-                    Mathf.Sin((Time.time + i) * 4.0f) * 0.02f,
-                    Mathf.Cos((Time.time + i) * 4.0f) * 0.02f,
+                    Mathf.Sin((Time.time + i) * (_var1 + Mathf.Sin(Time.time) * (_var1 / 3.0f))) * _var2,
+                    Mathf.Cos((Time.time + i) * (_var1 + Mathf.Sin(Time.time) * (_var1 / 3.0f))) * _var2,
                     distance * i
                 );
 	        }
